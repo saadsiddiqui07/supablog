92b6f43c59546114326ff7583a5159abed80f10d
saadsiddiqui07 <saadsiddiqui927@gmail.com>
2022-06-30 00:53:19 +0530

    Add type annotaions to the variables

--- client.ts
+++ client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabase_key: string = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const supabase_url: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 
 export const supabase = createClient(supabase_url, supabase_key);
